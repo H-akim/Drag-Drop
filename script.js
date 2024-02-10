cf2fb729f5a6285ca3f1b21d8166d33f9362e9c0
H-akim <92632018+H-akim@users.noreply.github.com>
2024-02-10 14:51:15 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+var lists = document.getElementsByClassName("list");
+var rightBox = document.getElementById("right");
+var leftBox = document.getElementById("left");
+
+for(list of lists){
+    list.addEventListener("dragstart", function(e){
+        let selected = e.target;
+        rightBox.addEventListener("dragover", function(e){
+            e.preventDefault();
+        });
+        rightBox.addEventListener("drop", function(e){
+            rightBox.appendChild(selected);
+            selected = null;
+        });
+        leftBox.addEventListener("dragover", function(e){
+            e.preventDefault();
+        });
+        leftBox.addEventListener("drop", function(e){
+            leftBox.appendChild(selected);
+            selected = null;
+        });
+    });
+};
